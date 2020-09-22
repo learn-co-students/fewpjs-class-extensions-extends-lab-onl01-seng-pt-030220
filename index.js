c2215bd109c6bb25bd02ee9055366c45ca20a7fb
AHMED AFANDY <eng.computer1@yahoo.com>
2020-09-22 02:32:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+
+class Polygon {
+
+    constructor(countSides){
+      this._countSides = countSides;
+    }
+
+    get countSides(){
+      return this._countSides = (this._countSides).length
+    }
+
+    get perimeter() {
+      return this._countSides = this._countSides.reduce((acc,element) =>acc+element)
+    }
+
+}
+
+
+
+class Triangle  extends Polygon {
+
+    get isValid()
+    {
+
+      return  this. _countSides == 3 ? true:false
+    }
+
+    get perimeter() {
+       if (typeof(this._countSides) === "object")
+       {
+       return this._countSides = this._countSides.reduce((acc,element) =>acc+element)
+       }else{
+        return this._countSides = this._countSides * 5
+       }
+
+    }
+
+  }
+
+
+
+  class Square  extends Polygon {
+
+    get isValid()
+    {
+      return  this. _countSides == 4 ? true:false
+    }
+
+    get area() {
+
+      return this._countSides = this._countSides[0] * this._countSides[1]
+    }
+
+
+    }
